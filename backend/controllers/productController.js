ab4ee8631c609c40420687973d0658a95a60cc88
soumya <smdas989@gmail.com>
2023-07-15 16:25:27 +0530

    [ADDED] get all products api

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -2,7 +2,7 @@ const Product = require("../models/productModels")
 
 
 
-//Create Product
+//Create Product - Admin
 exports.createProduct = async(req, res, next)=>{
     const product = await Product.create(req.body);
 
@@ -12,8 +12,11 @@ exports.createProduct = async(req, res, next)=>{
     })
 }
 
-exports.getAllProducts = (req, res) => {
-    console.log("here")
-    res.status(200).json({message: "Route is working fine"})
+// Get All product
+exports.getAllProducts = async(req, res) => {
+    const products = await Product.find();
+    res.status(200).json({
+        success: true, 
+        products})
 
 }
